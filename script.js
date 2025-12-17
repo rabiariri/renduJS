e1bbf5ee8bf52772a5ae3fca7533749217fca163
Sankilicoglur <rabiasankilicoglu@gmail.com>
2025-12-17 15:11:20 +0100

    majjjjs

---
+++ script.js
@@ -0,0 +1,35 @@
+const apiUrl = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
+
+fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // CODE QUI S'EXECUTE EN PRENANT LES INFORMATIONS DU JSON
+        textBanner(data);
+
+    })
+    .catch(function (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+    });
+
+function textBanner(data) {
+    const banner = document.getElementById("banner");
+
+    // CREATION TEXTE BANNIERE
+    const textCenterBanner = document.createElement("div");
+    textCenterBanner.classList.add("text-banner");
+
+    const titleBanner = document.createElement("h1");
+    titleBanner.textContent = data.nomCommercial;
+
+    const paraBanner = document.createElement("p");
+    paraBanner.textContent = data.phraseAccroche;
+
+    // AFFILIATION DU CONTENU CREE A LA DIV CREEE
+    textCenterBanner.appendChild(titleBanner);
+    textCenterBanner.appendChild(paraBanner);
+
+    // AFFICHAGE TEXTE BANNIERE EN RELIANT LA DIV A LA VARIABLE HTML
+    banner.appendChild(textCenterBanner);
+}
