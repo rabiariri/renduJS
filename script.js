34e2ab998f3c5f39092d92c6ab578446c4f5237a
Sankilicoglur <madokaontop5@gmail.com>
2026-01-09 11:45:07 +0100

    acceuil

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const apiUrl = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
 
 fetch(apiUrl)
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
+
+        // récuperation dans le dom
+        const banniere = document.getElementById("banner");
+
+        // création Text 
+        const texteBanniere = document.createElement("div");
+        texteBanniere.classList.add("text-banner");
+
+        const hbanniere = document.createElement("h1");
+        hbanniere.textContent = data.nomCommercial;
+
+        const pbanniere = document.createElement("p");
+        pbanniere.textContent = data.phraseAccroche;
+
+        const btnbanniere = document.createElement("button");
+        btnbanniere.textContent = data.texteAppelAction;
+
+        texteBanniere.appendChild(hbanniere);
+        texteBanniere.appendChild(pbanniere);
+        texteBanniere.appendChild(btnbanniere);
+
+        banniere.appendChild(texteBanniere);
         
 
 
 
 
+
     })
-    .catch(function(error) {
+    .catch(function (error) {
         console.error("Erreur lors de la récupération des données :", error);
     });
